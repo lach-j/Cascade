2ad527fa1e8861b48cd7aed66860e112cfe64517
Lachlan Johnson <Lachlan.Johnson@consultationmanager.com>
2025-03-22 15:10:26 +1100

    add tests

---
+++ Flagsmith.Testing/UnitTests/Authentication/AuthenticationMiddlewareTests.cs
@@ -0,0 +1,86 @@
+﻿using Flagsmith.Core;
+using Flagsmith.Core.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Flagsmith.Testing.UnitTests.Authentication;
+
+public class AuthenticationMiddlewareTests
+{
+    [Fact]
+    public async Task InvokeAsync_WhenRequireAuthenticationIsFalse_CallsNext()
+    {
+        // Arrange
+        var next = new RequestDelegate(_ => Task.CompletedTask);
+        var middleware = new FlagsmithAuthenticationMiddleware(next);
+        var context = new DefaultHttpContext();
+        var serviceProvider = new ServiceCollection().BuildServiceProvider();
+        var options = new FlagsmithOptions { RequireAuthentication = false };
+
+        // Act
+        await middleware.InvokeAsync(context, serviceProvider, options);
+
+        // Assert
+        Assert.Equal(StatusCodes.Status200OK, context.Response.StatusCode);
+    }
+    
+    [Fact]
+    public async Task InvokeAsync_WhenRequireAuthenticationIsTrueAndNoAuthProvider_ReturnsInternalServerError()
+    {
+        // Arrange
+        var next = new RequestDelegate(_ => Task.CompletedTask);
+        var middleware = new FlagsmithAuthenticationMiddleware(next);
+        var context = new DefaultHttpContext();
+        var serviceProvider = new ServiceCollection().BuildServiceProvider();
+        var options = new FlagsmithOptions { RequireAuthentication = true };
+
+        // Act
+        await middleware.InvokeAsync(context, serviceProvider, options);
+
+        // Assert
+        Assert.Equal(StatusCodes.Status500InternalServerError, context.Response.StatusCode);
+    }
+    
+    [Fact]
+    public async Task InvokeAsync_WhenRequireAuthenticationIsTrueAndAuthProviderReturnsTrue_CallsNext()
+    {
+        // Arrange
+        var next = new RequestDelegate(_ => Task.CompletedTask);
+        var middleware = new FlagsmithAuthenticationMiddleware(next);
+        var context = new DefaultHttpContext();
+        var serviceProvider = new ServiceCollection()
+            .AddSingleton<IAuthenticationProvider>(new BasicAuthenticationProvider("username", "password"))
+            .BuildServiceProvider();
+        var options = new FlagsmithOptions { RequireAuthentication = true };
+
+        context.Request.Headers["Authorization"] = "Basic " + Convert.ToBase64String("username:password"u8.ToArray());
+
+        // Act
+        await middleware.InvokeAsync(context, serviceProvider, options);
+
+        // Assert
+        Assert.Equal(StatusCodes.Status200OK, context.Response.StatusCode);
+    }
+    
+    [Fact]
+    public async Task InvokeAsync_WhenRequireAuthenticationIsTrueAndAuthProviderReturnsFalse_ReturnsUnauthorized()
+    {
+        // Arrange
+        var next = new RequestDelegate(_ => Task.CompletedTask);
+        var middleware = new FlagsmithAuthenticationMiddleware(next);
+        var context = new DefaultHttpContext();
+        var serviceProvider = new ServiceCollection()
+            .AddSingleton<IAuthenticationProvider>(new BasicAuthenticationProvider("username", "password"))
+            .BuildServiceProvider();
+        
+        var options = new FlagsmithOptions { RequireAuthentication = true };
+
+        context.Request.Headers.Authorization = "Basic " + Convert.ToBase64String("invalid:invalid"u8.ToArray());
+
+        // Act
+        await middleware.InvokeAsync(context, serviceProvider, options);
+
+        // Assert
+        Assert.Equal(StatusCodes.Status401Unauthorized, context.Response.StatusCode);
+    }
+}
---
+++ Flagsmith.Testing/UnitTests/Authentication/BasicAuthenticationProviderTests.cs
@@ -0,0 +1,81 @@
+﻿using Flagsmith.Core.Authentication;
+using Microsoft.AspNetCore.Http;
+
+namespace Flagsmith.Testing.UnitTests.Authentication;
+
+public class BasicAuthenticationProviderTests
+{
+    [Fact]
+    public async Task AuthenticateAsync_WhenInvalidAuthorizationHeader_ReturnsFalse()
+    {
+        // Arrange
+        var provider = new BasicAuthenticationProvider("username", "password");
+        var context = new DefaultHttpContext();
+        context.Request.Headers.Authorization = "Invalid";
+
+        // Act
+        var result = await provider.AuthenticateAsync(context);
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public async Task AuthenticateAsync_WhenNoAuthorizationHeader_ReturnsFalse()
+    {
+        // Arrange
+        var provider = new BasicAuthenticationProvider("username", "password");
+        var context = new DefaultHttpContext();
+
+        // Act
+        var result = await provider.AuthenticateAsync(context);
+
+        // Assert
+        Assert.False(result);
+    }
+    
+    [Fact]
+    public async Task AuthenticateAsync_WhenInvalidFormat_ReturnsFalse()
+    {
+        // Arrange
+        var provider = new BasicAuthenticationProvider("username", "password");
+        var context = new DefaultHttpContext();
+        context.Request.Headers.Authorization = $"Basic {Convert.ToBase64String("invalid:invalid:invalid"u8.ToArray())}";
+
+        // Act
+        var result = await provider.AuthenticateAsync(context);
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public async Task AuthenticateAsync_WhenInvalidCredentials_ReturnsFalse()
+    {
+        // Arrange
+        var provider = new BasicAuthenticationProvider("username", "password");
+        var context = new DefaultHttpContext();
+        context.Request.Headers.Authorization = "Basic " + Convert.ToBase64String("invalid:invalid"u8.ToArray());
+
+        // Act
+        var result = await provider.AuthenticateAsync(context);
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public async Task AuthenticateAsync_WhenValidCredentials_ReturnsTrue()
+    {
+        // Arrange
+        var provider = new BasicAuthenticationProvider("username", "password");
+        var context = new DefaultHttpContext();
+        context.Request.Headers["Authorization"] = "Basic " + Convert.ToBase64String("username:password"u8.ToArray());
+
+        // Act
+        var result = await provider.AuthenticateAsync(context);
+
+        // Assert
+        Assert.True(result);
+    }
+}
